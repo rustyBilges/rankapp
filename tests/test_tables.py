568569ffaf6e4060d4fdf2724aaf177a7d7f3b5d
Rusty <cm1788@bristol.ac.uk>
2019-02-26 13:19:54 +0100

    Written tests for table 2 submission. Failing tests.

--- tests/test_tables.py
+++ tests/test_tables.py
@@ -1,10 +1,10 @@
 import pytest 
 
-## need to tests the following WITH user logged in also..
 @pytest.mark.parametrize('path', (
     '/',
     '/table2',
     '/submit_table1'
+    '/submit_table2'
 ))
 def test_login_required(client, path):
     response = client.get(path)
@@ -17,4 +17,10 @@ def test_submit_table1(client, auth):
     response = client.post('/submit_table1')
     assert response.headers['Location'] == 'http://localhost/table2'
     
+def test_submit_table2(client, auth):
+    ## test that it redirects to logout message AND 
+    ##              saves table 2 data correctly 
+    auth.login()
+    response = client.post('/submit_table2')
+    assert response.headers['Location'] == 'http://localhost/logout_msg'
     
